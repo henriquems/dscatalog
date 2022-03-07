0be26dc030f1a6c679c58664cf9a55f7254869a9
Henrique Magalhães Simões <henriquems@gmail.com>
2022-03-07 16:43:22 -0300

    Pesquisa customizada product

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ClientResources.java
@@ -0,0 +1,61 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscatalog.dto.ClientDTO;
+import com.devsuperior.dscatalog.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResources {
+	
+	@Autowired
+	private ClientService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+		Page<ClientDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+		ClientDTO clientDTO = service.findById(id);
+		return ResponseEntity.ok().body(clientDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> save(@RequestBody ClientDTO dto) {
+		dto = service.save(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
