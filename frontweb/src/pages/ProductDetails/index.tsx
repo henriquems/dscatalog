98f90baa7a9e8965bd9438adcc4d1a72ed5e4f2f
Henrique <henriquems@gmail.com>
2022-03-15 23:58:53 -0300

    Primeira requisição axios

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,22 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 import './styles.css';
 
 const ProductDetails = () => {
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(BASE_URL + "/products/2")
+    .then(response => {
+      setProduct(response.data);
+    });
+  }, []);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -17,23 +30,20 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
-                alt="Nome do produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do produto</h2>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-                neque, voluptatibus ad quas error minima nisi esse. A vitae
-                odio, animi quia reprehenderit, aliquid dolorum modi laboriosam
-                corporis dolor quae.
+                {product?.description}
               </p>
             </div>
           </div>
