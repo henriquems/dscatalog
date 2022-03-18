1a008de3e882864df3b257639fec478054f7875f
Henrique Magalhães Simões <henriquems@gmail.com>
2022-03-18 10:12:26 -0300

    Login

---
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,31 @@
+import {ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
+import { Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import './styles.css';
+
+const Auth = () => {
+    return (
+        <div className="auth-container"> 
+            <div className="auth-banner-container">
+                <h1>Divulgue seus produtos no DsCatalog</h1>
+                <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos</p>
+                <AuthImage />
+            </div>
+            <div className='auth-form-container'>
+                <Switch>
+                    <Route path="/admin/auth/login">
+                        <Login />
+                    </Route>
+                    <Route path="/admin/auth/signup">
+                        <h1>Card signup</h1>
+                    </Route>
+                    <Route path="/admin/auth/recover">
+                        <h1>Card recover</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
