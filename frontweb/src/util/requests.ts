49e428c25b867aa6a6af0d2c8a3d2c992d8f7222
Henrique Magalhães Simões <henrique.simoes@feluma.org.br>
2022-03-22 11:23:27 -0300

    Oraganição do arquivo util/request

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,7 +1,7 @@
+import { getAuthData } from './storage';
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
-import jwtDecode from 'jwt-decode';
 
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
@@ -9,29 +9,10 @@ export const BASE_URL =
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
-type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
-
-export type TokenData = {
-  exp: number;
-  user_name: string;
-  authorities: Role[];
-}
-
 type LoginData = {
   username: string;
   password: string;
 };
-
-type LoginResponse = {
-  access_token: string;
-  token_type: string;
-  expires_in: number;
-  scope: string;
-  userFirstName: string;
-  userId: number;
-};
-
-const tokenKey = 'authData';
 
 export const requestBackendLogin = (loginData: LoginData) => {
   const headers = {
@@ -53,56 +34,36 @@ export const requestBackendLogin = (loginData: LoginData) => {
   });
 };
 
-export const saveAuthData = (obj: LoginResponse) => {
-  localStorage.setItem(tokenKey, JSON.stringify(obj));
-};
-
-export const getAuthData = () => {
-  const str = localStorage.getItem(tokenKey) ?? '{}';
-  return JSON.parse(str) as LoginResponse;
-};
-
-export const removeAuthData = () => {
-  localStorage.removeItem(tokenKey);
-}
-
 export const requestBackend = (config: AxiosRequestConfig) => {
   const headers = config.withCredentials
     ? {
-      ...config.headers,
-      Authorization: 'Bearer' + getAuthData().access_token,
-    }
+        ...config.headers,
+        Authorization: 'Bearer' + getAuthData().access_token,
+      }
     : config.headers;
 
   return axios({ ...config, baseURL: BASE_URL, headers });
 };
 
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  if (error.response.status === 401 || error.response.status === 403) {
-    history.push('/admin/auth');
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      history.push('/admin/auth');
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
-
-export const getTokenData = () : TokenData | undefined => {
-  try {
-    return jwtDecode(getAuthData().access_token) as TokenData;
-  } catch (error) {
-    return undefined;
-  }
-}
-
-export const isAuthenticated = () : boolean => {
-  const tokenData = getTokenData();
-  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
-}
+);
